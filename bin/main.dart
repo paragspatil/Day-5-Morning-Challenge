62c81fee4f0224813f5729926916198184523290
Parag <paragspatil123@gmail.com>
2019-10-07 14:44:04 +0530

    assingnment complete

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,6 @@
 // Challenge One:
+
+import 'dart:io';
 // Create a function 'squarePatch' that takes an integer and prints an n x n square solely consisting of the integer n.
 //squarePatch(3) should print
 //  [
@@ -6,7 +8,61 @@
 //    [3, 3, 3],
 //    [3, 3, 3]
 //  ]
-//
+//void squarePatch(int x){
+  //int i = 0;
+  //List l = List(x);
+  //for(i=0; i<x; i++){
+  //  l[i] = x;
+  //}
+  //for(i=0; i<x; i++){
+    //print("$l ,");
+  //}
+void squarePatch(int x){
+  int i = 0;
+  int j = 0;
+  List<List<int>> l = new List.filled(x, []);
+
+  for(j=0; j<x; j++) {
+    List <int> temp = [];
+    for (i = 0; i < x; i++) {
+      temp.add(x);
+    }
+    l[j] = temp;
+
+  }
+  print(l);
+}
+
+
+//}
+void printBoard(List n){
+  String a = n[0][0];
+  String b = n[0][1];
+  String c = n[0][2];
+  String d = n[1][0];
+  String e = n[1][1];
+  String f = n[1][2];
+  String g= n[2][0];
+  String h= n[2][1];
+  String i = n[2][2];
+
+
+
+
+
+  print('$a | $b | $c');
+  print('---+----+---');
+  print('$d | $e | $f');
+  print('---+----+---');
+  print('$g | $h | $i');
+}
+
+
+
+
+
+
+
 //squarePatch(5) should print
 //  [
 //    [5, 5, 5, 5, 5],
@@ -32,6 +88,7 @@ main() {
   ];
 
   printBoard(board);
+  squarePatch(5);
   // The above call should print:
   //    A   B   C
   // 1  O |   |
@@ -41,6 +98,8 @@ main() {
   // 3  X |   |
 
   board[0][2] = 'O';
+
+
   printBoard(board);
   // The above call should print:
   //    A   B   C
